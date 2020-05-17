db10c5f20a123f61238e57d5e053542b6535b983
Tomohito Ozaki <ozaki@yuroyoro.com>
2020-05-17 11:57:53 +0900

    rand

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate rand;
 
 use std::char;
+use std::env;
 use rand::Rng;
 
 fn generate(n: u32) -> String {
@@ -11,5 +12,22 @@ fn generate(n: u32) -> String {
 }
 
 fn main() {
-    println!("{}", generate(140));
+    let args: Vec<String> = env::args().collect();
+
+    match args.get(1) {
+        Some(cmd) => {
+            match &**cmd {
+                "rand" => {
+                    let n: u32 = args.get(2).and_then( |s| { s.trim().parse().ok() }).unwrap_or(140);
+                    println!("{}", generate(n));
+                },
+                _ => {
+                    println!("unknow option ; {}", cmd);
+                }
+            }
+        },
+        None => {
+          println!("args required ");
+        }
+    }
 }
